c1f8258f4622f545fcf9d11a06d5a6221e04a804
vinod5838 <108979624+vinod5838@users.noreply.github.com>
2022-07-17 14:38:57 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -12,7 +12,6 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, for webhooks testing purpose");
     System.out.println("Hello, for webhooks testing purpose");
-    System.out.println("Hello, for webhooks testing purpose");
     System.out.println("Hello, devops guy are you there");
   }
 }
